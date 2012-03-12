b5fbf0f0400c9430006b568ef1dd6bcbd7ba34cb
Obexer Christoph <cobexer@gmail.com>
2012-03-12 21:33:32 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,6 @@
+5 in a row
+==========
+
+The original purpose of this project is to play with the canvas and websocket API's.
+
+let's see what happens^^
---
+++ src/ui.js
@@ -0,0 +1,8 @@
+/**
+ * 5-in-a-row <http://cobexer.github.com/5-in-a-row/>
+ * Copyright (c) 2012 Obexer Christoph. All rights reserved.
+ *
+ * Released under the MIT and GPL licenses.
+ */
+
+
